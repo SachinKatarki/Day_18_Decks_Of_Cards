8b30d24ba8d83d8064cabc316488f1738b9b8c6d
Textspot <sachinkatarki1995@gmail.com>
2021-11-24 16:38:31 +0530

    Added: Make a function to shuffle the cards and Distributed the cards

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DeckOfCards {
-	  Scanner scanner = new Scanner(System.in);
+	 Scanner scanner = new Scanner(System.in);
 	    public ArrayList<String> cardsDeck = new ArrayList<String>();
 
 	    public ArrayList<String> makeADeck() {
@@ -24,6 +24,7 @@ public class DeckOfCards {
 
 
 	    public static void toshuffle(ArrayList<String> cardsDeck) {
+	        System.out.println("shuffling the cards before Distribution");
 	        ArrayList<String> temp = new ArrayList<String>();
 	        while (!cardsDeck.isEmpty()) {
 	            int loc = (int) (Math.random() * cardsDeck.size());
@@ -31,6 +32,7 @@ public class DeckOfCards {
 	            cardsDeck.remove(loc);
 	        }
 	        cardsDeck = temp;
+	        toDisplay(cardsDeck);
 	    }
 
 	    private static void toDisplay(ArrayList<String> cardsDeck) {
@@ -46,6 +48,7 @@ public class DeckOfCards {
 	        if (player >= 2 && player <= 4) {
 	            System.out.println(player + " will players will  play the game");
 	            sequenceOfPlay(player);
+
 	        } else {
 	            System.out.println("please enter number of players in the Range");
 	            this.noOfPlayers();
@@ -55,6 +58,7 @@ public class DeckOfCards {
 	    public void sequenceOfPlay(int player) {
 	        for (int i = 1; i < player; i++) {
 	            System.out.println("Player " + i + " Getting card:");
+	            toshuffle(cardsDeck);
 	        }
 	    }
 
@@ -62,7 +66,7 @@ public class DeckOfCards {
 	        System.out.println("Welcome to Deck of Cards Program");
 	        DeckOfCards deck = new DeckOfCards();
 	        deck.makeADeck();
-	        deck.noOfPlayers();	    
+	        deck.noOfPlayers();
 	    }
 
 }
